c14fa98da04e0e75120f5938aea9ffc008db81f5
Zenilton Pereira <zeniltonp0@gmail.com>
2025-09-01 11:24:12 -0300

    TODO-1: resolvendo erro no ExampleTest

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Livewire\Auth\Register;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('dashboard');
+});
+
 Route::middleware('guest')->group(function () {
     Route::get('/register', Register::class)->name('register');
 });
