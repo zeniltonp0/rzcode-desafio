92502ad4d3c24b95dde16c51e0101d1df20030b4
Zenilton Pereira <zeniltonp0@gmail.com>
2025-09-01 20:13:05 -0300

    TODO-5: criando relacionamentos dos models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
     use SoftDeletes;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
