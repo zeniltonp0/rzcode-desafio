a4f50105fe840ff734ef7f8b437c4c67c8b77a0f
Zenilton Pereira <zeniltonp0@gmail.com>
2025-09-01 13:12:45 -0300

    TODO-1: adicionando validacao nos campos do registro

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -5,15 +5,19 @@ namespace App\Livewire\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 #[Layout('layouts.guest')]
 class Register extends Component
 {
+    #[Rule(['required', 'max:255', 'string'])]
     public ?string $name = null;
 
+    #[Rule(['required', 'max:255', 'email', 'string', 'unique:users,email'])]
     public ?string $email = null;
 
+    #[Rule(['required', 'string', 'min:8', 'same:password_confirmation'])]
     public ?string $password = null;
 
     public ?string $password_confirmation = null;
