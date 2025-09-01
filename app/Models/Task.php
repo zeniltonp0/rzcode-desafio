ae25ece28732cf188352f8a523ad062b66c5978c
Zenilton Pereira <zeniltonp0@gmail.com>
2025-09-01 20:27:44 -0300

    TODO-5: criando seeder, factory e povoando o banco de dados

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,13 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'user_id',
+        'description',
+        'due_date',
+        'completed',
+    ];
+
+    protected $casts = [
+        'due_date' => 'datetime',
+    ];
 
     public function user(): BelongsTo
     {
---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(4),
+            'description' => fake()->sentence(),
+            'due_date' => fake()->dateTimeBetween('+1 day', '+1 month'),
+            'completed' => fake()->boolean(),
+        ];
+    }
+}
