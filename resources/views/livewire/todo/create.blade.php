0d5ee2f61f64c194a28e72c8521c3bdf56037219
Zenilton Pereira <zeniltonp0@gmail.com>
2025-09-02 20:49:21 -0300

    TODO-7: criando tarefa no banco de dados

--- resources/views/livewire/todo/create.blade.php
+++ resources/views/livewire/todo/create.blade.php
@@ -1,8 +1,15 @@
 <div>
-    @if ($modal)
-        <x-modal title="Criar uma tarefa" wire center>
-            Criar uma tarefa
-        </x-modal>
-    @endif
+    <x-modal title="Criar uma tarefa" wire center>
+        <form wire:submit="save" id="create-task-form">
+            <div class="space-y-4">
+                <x-input label="Título" wire:model="title"/>
+                <x-textarea label="Descrição" wire:model="description" resize-auto/>
+                <x-date label="Prazo" format="DD/MM/YYYY" wire:model="due_date"/>
+            </div>
+        </form>
+        <x-slot:footer>
+            <x-button form="create-task-form" type="submit" text="Criar" primary md wire:loading.attr="disabled" />
+        </x-slot:footer>
+    </x-modal>
 </div>
 
