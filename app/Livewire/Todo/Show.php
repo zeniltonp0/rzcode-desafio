b3195dda278e8de23c55aa50b705c9bc0380cb80
Zenilton Pereira <zeniltonp0@gmail.com>
2025-09-02 10:21:18 -0300

    TODO-5: frontend: renderizando os cards de cada tarefa

--- app/Livewire/Todo/Show.php
+++ app/Livewire/Todo/Show.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire\Todo;
 
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Show extends Component
 {
+    #[Computed]
+    public function tasks()
+    {
+        return auth()->user()->tasks;
+    }
+
     public function render()
     {
         return view('livewire.todo.show');
--- resources/views/livewire/todo/show.blade.php
+++ resources/views/livewire/todo/show.blade.php
@@ -1,3 +1,8 @@
-<div>
-    lista de tarefas
+<div class="space-y-2">
+    @foreach ($this->tasks as $task)
+        <x-card header="{{ $task->title }}">
+            {{ $task->description }}
+            {{ $task->due_date->format('d/m/Y') }}
+        </x-card>
+    @endforeach
 </div>
